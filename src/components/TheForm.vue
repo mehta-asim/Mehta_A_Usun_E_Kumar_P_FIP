3c7bf846907b253e2fcb37021c56917af6f5d7f8
mehta-asim <72264877+mehta-asim@users.noreply.github.com>
2022-03-29 19:37:33 -0400

    edited the form component adding new functionality

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="form">
-    <form action="#">
+    <form>
       <input type="text" name="first_name" placeholder="First Name" />
       <input type="text" name="last_name" placeholder="Last Name" />
       <input type="email" name="email" placeholder="Email" />
@@ -11,18 +11,26 @@
         rows="10"
         placeholder="your message here..."
       ></textarea>
-      <input
-        type="number"
-        name="donation_amount"
-        placeholder="Donation Amount"
-      />
-      <select name="currency" id="currency">
-        <option value="cad">CAD</option>
-        <option value="usd">USD</option>
+      <select name="role" id="role">
+        <option value="Web Developer">Web Developer</option>
+        <option value="Content Editor">Content Editor</option>
+        <option value="Proofreader">Proofreader</option>
+        <option value="Content Verifier">Content Verifier</option>
       </select>
 
-      <button type="submit">Donate</button>
+      <button type="submit" @click="trySubmit">Donate</button>
     </form>
   </section>
 </template>
+
+<script>
+export default {
+  name: "TheForm",
+
+  methods: {
+    trySubmit() {},
+  },
+};
+</script>
+
 
